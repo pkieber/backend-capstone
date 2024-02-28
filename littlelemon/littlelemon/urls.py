4cbeaa359382e79a2062690f94881c8025080c24
pkieber <113725311+pkieber@users.noreply.github.com>
2024-02-28 17:25:59 +0100

    add booking API

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import path, include  
- 
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
+
+# Define a router for registering viewsets
+router = DefaultRouter()
+router.register(r'bookings', BookingViewSet) 
+
+
 urlpatterns = [ 
     path('admin/', admin.site.urls), 
     path('restaurant/', include('restaurant.urls')),
     #path('reservation/', include('reservation.urls')),
     path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
+
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import MenuItem
-from .serializers import MenuItemSerializer #, BookingSerializer, UserSerializer
+from rest_framework import generics, viewsets, permissions
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 
+# Create a class-based view for the User model
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+# Create a class-based view for the Booking model
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
 # Create a class-based view for the Menu model
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
