df3bc6d59206435ae129b8fb5b4f1082f5caf7e7
Pat Kieber <113725311+pkieber@users.noreply.github.com>
2024-03-04 11:14:57 +0100

    Update settings.py

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'littlelemon',  
         'USER': 'root',  
-        'PASSWORD': 'ROO2drei4!',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
